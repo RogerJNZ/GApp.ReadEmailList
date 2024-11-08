b20859763c638c55a2f435027eef1b0ef1377f05
RogerJ <rogerj.nz@gmail.com>
2024-11-09 12:33:42 +1300

    Update ReadEmailList.js
    Refactor conditional formatting

--- ReadEmailList.js
+++ ReadEmailList.js
@@ -19,28 +19,44 @@ Read Email Messages
   
  Updates
  27 Oct 2024  RJ  Create EmailList worksheet if it doesn't exist. Force update of sheet after every block
- 04 Nov 2024  RJ  Refactored code. Added Gmail Search column
+ 04 Nov 2024  RJ  Refactored to reduce code. Added Gmail Search column
  08 Nov 2024  RJ  Changed Size to MB and added conditional formatting to highlight large Emails
+ 09 Nov 2024  RJ  Refactor conditional formatting 
 */
 
 function readEmailMessages() {
-  var logging = false;       // Turn off logging for faster processing
-  var threadPage = 0;        // 0 is first page
+  var logging         = false;       // Turn off logging for faster processing
+  var threadPage      = 0;        // 0 is first page
   var threadBlockSize = 50;  // Number of threads to read before writing to the spreadsheet
-  var newThread = "";        // Flag new a new Thread has started
-  // Sheet Details
-  var header = [["Page","Thread", "#","From","To", "Date","Subject", "Size (MB)","New Thread", "Gmail Search"]];
-  // Use the value in this field to search for the related email in Gmail  
-  var gmailSearch = '=INDIRECT("G"&ROW()) & " after:" & TEXT(INDIRECT("F"&ROW())-1,"YYYY/MM/DD") & " before:" & TEXT(INDIRECT("F"&ROW())+1,"YYYY/MM/DD")';
-
+  var newThread       = "";        // Flag new a new Thread has started
+  // Sheet specific variables
+  var header          = [["Page","Thread", "#","From","To", "Date","Subject", "Size (MB)","New Thread", "Gmail Search"]];
+  var gmailSearch     = '=INDIRECT("G"&ROW()) & " after:" & TEXT(INDIRECT("F"&ROW())-1,"YYYY/MM/DD") & " before:" & TEXT(INDIRECT("F"&ROW())+1,"YYYY/MM/DD")';  // Use the value in this field to search for the related email in Gmail  
+  var condRuleValues  = [{value:10, colour:'#f4c7ce'}, {value:4, colour:'#fce8b2'}] // An email > 4MB probably has one or more photos attached 
+  
   // Get the EmailList sheet or create a new one
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("EmailList");
   if (sheet == null) {
     var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet("EmailList");
+    
+    // Highlight the larger emails 
+    lastRow = sheet.getLastRow();
+    var range = sheet.getRange("H:H");
+    var rules = []; //sheet.getConditionalFormatRules();
+    for (var i = 0; i < condRuleValues.length; i++) {
+      var rule = SpreadsheetApp.newConditionalFormatRule()
+        .whenNumberGreaterThanOrEqualTo(condRuleValues[i].value)
+        .setBackground(condRuleValues[i].colour)
+        .setRanges([range])
+        .build();        
+      rules.push(rule);
+    }
+    sheet.setConditionalFormatRules(rules);
   }
   Logger.log("Started  ---------------------");
  
-  // Restart from the last line or print the header if there are no lines
+
+  // Continue from the last line or print the header if if doesn't exist
   var line = sheet.getLastRow();
   if (line > 1 ) {
     var data = sheet.getRange(line, 1, 1, sheet.getLastColumn()).getValues();  
@@ -48,26 +64,9 @@ function readEmailMessages() {
   } else {
     sheet.getRange(1, 1, 1, header[0].length).setValues(header).setFontWeight("bold");
     SpreadsheetApp.flush(); // Force write 
-    var rules = []; //sheet.getConditionalFormatRules();
-    lastRow = sheet.getLastRow();
-    var range = sheet.getRange("H:H");
-    var rule = SpreadsheetApp.newConditionalFormatRule()
-      .whenNumberGreaterThanOrEqualTo(10)
-      .setBackground("#f4c7ce")
-      .setRanges([range])
-      .build();        
-    rules.push(rule);
-    rule = SpreadsheetApp.newConditionalFormatRule()
-      .whenNumberGreaterThanOrEqualTo(4)
-      .setBackground("#fce8b2")
-      .setRanges([range])
-      .build();        
-    rules.push(rule);
-    sheet.setConditionalFormatRules(rules);
-    SpreadsheetApp.flush(); // Force write 
   }
   
-  // Read blocks of threads i.e. email + replies
+  // Read emails in blocks of threads i.e. email + replies
   for (;; threadPage++) {
     var threads = GmailApp.search('-in:trash size:1M', threadPage*threadBlockSize, threadBlockSize)
     if (threads.length == 0) { break; } // No more threads
@@ -81,7 +80,7 @@ function readEmailMessages() {
       newThread = "***";
       if (logging) { Logger.log("*   threadPage:" + threadPage + " Thread:" + threadCount + " Line: " + line); }
   
-      // Loop through the emails in a conversation thread
+      // Loop through the emails in a conversation thread 
       for (var i = 0; i < messages.length; i++) {
         emailSize =  Math.round(messages[i].getRawContent().length/1024/1024,2);
         var messagesDetails = [threadPage, threadCount, line++, messages[i].getFrom(), messages[i].getTo(), messages[i].getDate(), messages[i].getSubject(), emailSize, newThread, gmailSearch];
